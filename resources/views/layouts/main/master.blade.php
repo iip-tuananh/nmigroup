845fa5fe1656f93be565b489fc5d7c915e388bc6
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2022-11-17 14:39:23 +0700

    fixed

--- resources/views/layouts/main/master.blade.php
+++ resources/views/layouts/main/master.blade.php
@@ -469,6 +469,36 @@
    @yield('js')
    <div id="fb-root"></div>
    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v15.0" nonce="zW87aZjL"></script>
+   <!-- Messenger Plugin chat Code -->
+   <div id="fb-root"></div>
+
+   <!-- Your Plugin chat code -->
+   <div id="fb-customer-chat" class="fb-customerchat">
+   </div>
+
+   <script>
+   var chatbox = document.getElementById('fb-customer-chat');
+   chatbox.setAttribute("page_id", "103984485775834");
+   chatbox.setAttribute("attribution", "biz_inbox");
+   </script>
+
+   <!-- Your SDK code -->
+   <script>
+   window.fbAsyncInit = function() {
+      FB.init({
+         xfbml            : true,
+         version          : 'v15.0'
+      });
+   };
+
+   (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+   </script>
    <div id="popupCartModal" class="modal fade" role="dialog">
    </div>
    <script>

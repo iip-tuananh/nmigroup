71b02d640a03a109166fc7173508d0f9425a4f6d
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2023-01-04 17:36:19 +0700

    fixed

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group(['namespace'=>'Client','middleware' => ['checkLanguage']], function
     Route::get('update-cart', 'CartController@update')->name('updateCart');
     Route::get('remove-from-cart', 'CartController@remove')->name('removeCart');
     Route::get('thanh-toan.html','CartController@checkout')->name('checkout');
-    Route::post('thantoan','CartController@postBill')->name('postBill');
+    Route::post('thanh-toan','CartController@postBill')->name('postBill');
 
     Route::get('dat-ban.html','PageController@orderNow')->name('orderNow');
     Route::get('menu.html','PageController@menu')->name('menu');
